2a5e8905f5536c40de02a3ea6e24015176f08a70
kellycouvrette <kellycouvrette@gmail.com>
2023-03-08 18:48:31 -0800

    created a bottle
    I created the milkCarton.js file and designed a bottle to be added to the bottle booth later

---
+++ thingamabobs/milkCarton.js
@@ -0,0 +1,62 @@
+import { defs, tiny } from "../examples/common.js";
+
+const {
+    Vector,
+    Vector3,
+    vec,
+    vec3,
+    vec4,
+    color,
+    hex_color,
+    Shader,
+    Matrix,
+    Mat4,
+    Light,
+    Shape,
+    Material,
+    Scene,
+} = tiny;
+
+export default class milkCarton {
+    shader = new defs.Phong_Shader();
+
+    shapes = {
+        body: new defs.Subdivision_Sphere(5),
+        neck: new defs.Rounded_Closed_Cone(50, 50),
+    };
+
+    default_transforms = {
+        body: Mat4.scale(0.15, 0.3, 0.25),
+        neck: Mat4.scale(0.15, 0.2, 0.2)
+            .times(Mat4.translation(0, 1.7, 0))
+            .times(Mat4.rotation((3 * Math.PI) / 2, 1, 0, 0)),
+    };
+
+    constructor() {}
+
+    draw(context, program_state, location, color) {
+        const body_transform = location.times(this.default_transforms.body);
+        const neck_transform = location.times(this.default_transforms.neck);
+        const body_material = new Material(this.shader, {
+            ambient: 1,
+            color,
+        });
+        const neck_material = new Material(this.shader, {
+            ambient: 0.8,
+            color,
+        });
+
+        this.shapes.body.draw(
+            context,
+            program_state,
+            body_transform,
+            body_material
+        );
+        this.shapes.neck.draw(
+            context,
+            program_state,
+            neck_transform,
+            neck_material
+        );
+    }
+}
