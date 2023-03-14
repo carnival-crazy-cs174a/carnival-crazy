cbbf6c65cd7397bf55e89d86beff94af49db973d
Paul Z <paulz@ucla.edu>
2023-03-13 18:32:33 -0700

    Pop balloons when hit by dart(#7)
    - Clean up `Dart` and `Balloon` class code
    - Calculate dart score (number of balloons popped)
    - Draw bounding volumes for debugging (currently commented out)

--- thingamabobs/balloon.js
+++ thingamabobs/balloon.js
@@ -18,32 +18,46 @@ const {
 } = tiny;
 
 export default class Balloon {
-  shader = new defs.Phong_Shader();
+  constructor(location, color) {
+    this.shader = new defs.Phong_Shader();
 
-  shapes = {
-    body: new defs.Subdivision_Sphere(5),
-    neck: new defs.Rounded_Closed_Cone(50, 50),
-  };
+    this.shapes = {
+      body: new defs.Subdivision_Sphere(5),
+      neck: new defs.Rounded_Closed_Cone(50, 50),
+    };
 
-  default_transforms = {
-    body: Mat4.scale(0.5, 0.6, 0.5),
-    neck: Mat4.scale(0.05, 0.05, 0.05)
-      .times(Mat4.translation(0, -12, 0))
-      .times(Mat4.rotation((3 * Math.PI) / 2, 1, 0, 0)),
-  };
+    this.default_transforms = {
+      body: Mat4.scale(0.5, 0.6, 0.5),
+      neck: Mat4.scale(0.05, 0.05, 0.05)
+        .times(Mat4.translation(0, -12, 0))
+        .times(Mat4.rotation((3 * Math.PI) / 2, 1, 0, 0)),
+    };
 
-  constructor() {}
+    this.bounding_volume = new defs.Subdivision_Sphere(2);
+    this.leeway = 0.1;
 
-  draw(context, program_state, location, color) {
-    const body_transform = location.times(this.default_transforms.body);
-    const neck_transform = location.times(this.default_transforms.neck);
+    this.location = location;
+    this.color = color;
+    this.location_and_transform = this.location.times(
+      this.default_transforms.body
+    );
+
+    this.wireframe_color = new Material(this.shader, {
+      color: hex_color("#ffffff"),
+      ambient: 1,
+    });
+  }
+
+  draw(context, program_state) {
+    const body_transform = this.location.times(this.default_transforms.body);
+    const neck_transform = this.location.times(this.default_transforms.neck);
     const body_material = new Material(this.shader, {
       ambient: 1,
-      color,
+      color: this.color,
     });
     const neck_material = new Material(this.shader, {
       ambient: 0.8,
-      color,
+      color: this.color,
     });
 
     this.shapes.body.draw(
@@ -58,5 +72,42 @@ export default class Balloon {
       neck_transform,
       neck_material
     );
+
+    /**
+     * Draw bounding volume for debugging
+     */
+    // const size = vec3(1 + this.leeway, 1 + this.leeway, 1 + this.leeway);
+    // this.bounding_volume.draw(
+    //   context,
+    //   program_state,
+    //   this.location_and_transform.times(Mat4.scale(...size)),
+    //   this.wireframe_color,
+    //   "LINE_STRIP"
+    // );
+  }
+
+  collides_with(other) {
+    // Cannot collide with itself
+    if (Object.is(this, other)) {
+      return false;
+    }
+
+    function intersect_sphere(p, margin = 0) {
+      return p.dot(p) < 1 + margin;
+    }
+
+    // `other` object must have a location_and_transform property
+    if (other.location_and_transform != null) {
+      const T = Mat4.inverse(this.location_and_transform).times(
+        other.location_and_transform,
+        Mat4.identity()
+      );
+
+      return this.bounding_volume.arrays.position.some((point) =>
+        intersect_sphere(T.times(point.to4(1)).to3(), this.leeway)
+      );
+    }
+
+    return false;
   }
 }
